2e53a21b8f2951d8c0a0b0d0a35bc9e0f3b62ba7
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-21 10:08:45 -0400

    migrating images

---
+++ src/components/pages/Display.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card1 from './Cards/Card1';
+import Card2 from './Cards/Card2';
+import Card3 from './Cards/Card3';
+import Card4 from './Cards/Card4';
+import Card5 from './Cards/Card5';
+import Card6 from './Cards/Card6';
+
+export default function Display() {
+  return (
+    <div>
+      <Card1/>
+      <Card2/>
+      <Card3/>
+      <Card4/>
+      <Card5/>
+      <Card6/>
+    </div>
+  );
+}
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import Card1 from './Cards/Card1';
-import Card2 from './Cards/Card2';
-import Card3 from './Cards/Card3';
-import Card4 from './Cards/Card4';
-import Card5 from './Cards/Card5';
-import Card6 from './Cards/Card6';
+import Display from './Display';
+
 
 export default function Project() {
   return (
@@ -25,12 +21,7 @@ export default function Project() {
         bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
         est ligula suscipit velit, sed bibendum turpis dui in sapien.
       </p>
-      <Card1/>
-      <Card2/>
-      <Card3/>
-      <Card4/>
-      <Card5/>
-      <Card6/>
+      <Display/>
     </div>
   );
 }
