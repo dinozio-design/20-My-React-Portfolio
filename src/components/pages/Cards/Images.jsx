cb0005341f7582351036c52f0fb0665c92860049
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-21 18:13:56 -0400

    all cards functional, all images working next is to update contents and links

--- src/components/pages/Cards/Images.jsx
+++ src/components/pages/Cards/Images.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 
 const IMAGES = {
     githubmark : new URL('../../../assets/images/githubmark.png', import.meta.url).href,
+    deploymark : new URL('../../../assets/images/deploymark.png', import.meta.url).href,
     image1 : new URL('../../../assets/images/img01.jpg', import.meta.url).href,
+    image2 : new URL('../../../assets/images/img02.jpg', import.meta.url).href,
+    image3 : new URL('../../../assets/images/img03.jpg', import.meta.url).href,
+    image4 : new URL('../../../assets/images/img04.jpg', import.meta.url).href,
+    image5 : new URL('../../../assets/images/img05.jpg', import.meta.url).href,
+    image6 : new URL('../../../assets/images/img06.jpg', import.meta.url).href,
+
 
 }
 
