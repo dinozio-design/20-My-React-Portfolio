ea9f04f6a5b9c7d11ea816c2d1316b192c4cf4fc
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-21 15:24:43 -0400

    cleaned up the contact page and comment fields

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -5,10 +5,8 @@ export default function Contact() {
   return (
     <div className='mt-4'>
       <h2>Contact Page</h2>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices.
+      <p className='mb-0'>
+        If you need to reach us, share your opinion, ask a question, or just simply say Hi, feel free to use the fields below. Be sure to type in your name and valid email address.
       </p>
       <div className='m-3 border border pt-3'>
         <Form />
