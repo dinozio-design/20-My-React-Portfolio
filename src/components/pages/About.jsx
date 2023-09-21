634a2cf4145b6e052622b8aa031aee54b04f44c7
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-21 14:14:50 -0400

    about me completed

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -4,14 +4,18 @@ export default function About() {
   return (
     <div className='mt-4'>
       <h2 >About Page</h2>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <div class="m-3 px-2">
+        <article className='text-start text-wrap mb-4 '>
+          This is where you can know a little more about me.
+          <p className='mt-3'>I am an inquisitive Engineer with a passion for AI solutions. I am redirecting my focus to Backend programming, DB architecture (SQL/NoSQL), and Computer Vision. Feel free to look me up on <a href="https://www.linkedin.com/in/sam-azimi-p-eng">LinkeIn</a>. I am open to connect, and I am always happy to help.</p>
+          <p>I just want to code, and create bespoke solutions to help businesses take advantage of AI, and become better, faster, and more cost effective 🙂</p>
+          <p className='mb-0 mt-1'>Name: Sam Azimi</p>
+          <p className='mb-0'>Title: Professional Engineer</p>
+          <p className='mb-0'>Occupation: Full Stack Developer</p>
+          <p className='mb-0'>Phone: +1-647-678-****</p>
+          <p className='mb-0'>email: sam@dinozio.design</p>
+        </article>
+      </div>
     </div>
   );
 }
