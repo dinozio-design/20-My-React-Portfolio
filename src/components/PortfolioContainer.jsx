8d651fbf2c5db9ef3aeb409c927511fd5265d6ab
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-21 08:54:59 -0400

    first set of wireframe completed with pages incorporated next is to develop the pages and style them

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -1,25 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
-
-export default function PortfolioContainer(){
+export default function PortfolioContainer() {
     const [currentPage, setCurrentPage] = useState('Home');
 
     // here we check to see what the value of currentPage is and then we return the corresponding componenet to render
-const renderPage = ()=>{
-    if (currentPage === 'Home') {
-        return <Home />;
-    }
-}
+    const renderPage = () => {
+        if (currentPage === 'Home') {
+            return <Home />;
+          }
+          if (currentPage === 'About') {
+            return <About />;
+          }
+          if (currentPage === 'Projects') {
+            return <Projects />;
+          }
+          return <Contact />;
+    };
 
-
-
+    const handlePageChange = (page) => setCurrentPage(page);
 
     return (
         <div>
             {/* passing the current page from state and the function to update it */}
-            <NavTabs currentPage = {currentPage} handlePageChange = {handlePageChange}/>
+            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+
             {/* next we call the renderPage methopde which will return a component */}
             {renderPage()}
         </div>
-    )
+    );
 }
