e7c036b9125cf0f2d6bab83836861efe010fbfd0
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-09-21 13:21:47 -0400

    futile efforts in adding gh logo.... moving on

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import ghLogo from '../assets/images/logo-github.svg'
+// import { GitHub} from "react-icons/fa";
 
 export default function Footer() {
   return (
     <div>
+      <hr/>
       <h4>My Resume:</h4>
-      <p className='text-body-secondary fst-italic fw-lighter'>It is still a work in progress, in the meanwhile feel free to check out my linkedIn at:  <a href="https://linkedin.com">https://linkedin.com</a> and my GitHub at: <a href="https://github.com">https://github.com</a> </p>
-      <p className='text-body-secondary fst-italic fw-lighter'>This web app is brought to you by:© Sam Azimi - 2023 CodeCamp Studen.Confidential and Proprietary. All Rights Reserved.</p>
+      <p className='text-body-secondary fst-italic fw-lighter'>It is still a work in progress, in the meanwhile feel free to check out my profiles on:</p><p> LinkedIn at:  <a href="https://www.linkedin.com/in/sam-azimi-p-eng">https://www.linkedin.com/in/sam-azimi-p-eng</a></p>
+      <p>  GitHub at: <a href="https://github.com/dinozio-design">https://github.com/dinozio-design</a> </p>
+      <p className='text-body-secondary fst-italic fw-lighter'>This web app is brought to you by:© Sam Azimi - 2023 CodeCamp Student. Confidential and Proprietary. All Rights Reserved.</p>
     </div>
   );
 }
